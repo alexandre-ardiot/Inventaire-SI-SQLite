864d11bf70d2c3f74f878ea6512982770739cc09
Alex <alex.ardiot@gmail.com>
2022-10-04 17:04:29 +0200

    Ajout de toutes les tables de la BDD

--- creer_bdd.py
+++ creer_bdd.py
@@ -3,6 +3,17 @@ import sqlite3
 connexion = sqlite3.connect('bdd.db')
 curseur = connexion.cursor()
 
+
+curseur.execute('''CREATE TABLE User
+                (
+                    id INTEGER PRIMARY KEY,
+                    role INTEGER,
+                    prenom TEXT,
+                    nom TEXT,
+                    mot_de_passe TEXT
+                )
+''')
+
 curseur.execute('''CREATE TABLE chat_tickets
                 (
                     date TEXT,
@@ -14,6 +25,33 @@ curseur.execute('''CREATE TABLE chat_tickets
                         ON DELETE CASCADE,
                     FOREIGN KEY (auteur)
                         REFERENCES User(id)
+                        ON DELETE CASCADE
+                )
+''')
+
+curseur.execute('''CREATE TABLE Ticket
+                (
+                    id INTEGER PRIMARY KEY,
+                    date_creation TEXT,
+                    id_ref_pret INTEGER,
+                    status INTEGER,
+                    message TEXT,
+                    FOREIGN KEY (id_ref_pret)
+                        REFERENCES Carnet_pret(reference_pc)
+                        ON DELETE CASCADE
+                )
+''')
+
+curseur.execute('''CREATE TABLE Carnet_pret
+                (
+                    reference_pc TEXT PRIMARY KEY,
+                    user_id INTEGER,
+                    type_ordinateur_id INTEGER,
+                    FOREIGN KEY (user_id)
+                        REFERENCES User(id)
+                        ON DELETE CASCADE,
+                    FOREIGN KEY (type_ordinateur_id)
+                        REFERENCES Type_ordinateur(id)
                         ON DELETE CASCADE
                 )
 ''')
