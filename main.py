01e713e1e5dc040e6da8b148225d310e7fc519c7
J-R59 <DucroJR@outlook.fr>
2022-10-11 11:40:06 +0200

    modif

--- main.py
+++ main.py
@@ -2,13 +2,6 @@ import sqlite3
 import crud
 import function
 
-<<<<<<< HEAD
-connexion = sqlite3.connect('bdd.db')
-curseur = connexion.cursor()
-
-
-=======
->>>>>>> e00d4e1adb12fed00f0d584e9a6410694144404f
 """
 choix_initial = input("Que voulez-vous faire :\n1 - Creer un compte \n2 - Se connecter\n\n")
 
@@ -26,11 +19,4 @@ else:
 
 crud.creation_utilisateur("Antoine", "Meresse", "abc")
 
-<<<<<<< HEAD
-connexion.commit()
-connexion.close()
-"""
-crud.creation_utilisateur("jr" , "nom" , "abc")
-=======
 crud.ajouter_pc ("eefdfjxck", 1 , None)
->>>>>>> e00d4e1adb12fed00f0d584e9a6410694144404f
